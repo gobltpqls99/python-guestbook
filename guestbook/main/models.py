c04fb1af2c15c3123e2df898eb5f313605d56aab
gobltpqls99 <gobltpqls99@naver.com>
2021-06-09 17:41:32 +0900

    reply

--- guestbook/main/models.py
+++ guestbook/main/models.py
@@ -13,3 +13,9 @@ class Post(models.Model):
     def __str__(self):
         return f"name : { self.name } , title: {self.postTitle}, content: {self.content[:10]}"
 
+class Reply(models.Model):
+    posting = models.ForeignKey(Post,on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=30)
+    pw = models.CharField(max_length=15)
+    created_at = models.DateTimeField(default=timezone.now)
+    reply = models.TextField()
--- guestbook/main/urls.py
+++ guestbook/main/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('write/', views.write, name='write'),
     path('<int:pk>/', views.posting, name="posting"),
-    path('delete/<int:pk>', views.delete_posting, name="delete")
+    path('delete/<int:pk>', views.delete_posting, name="delete"),
+    path('<int:pk>/comment/', views.reply, name="reply"),
+    path('reply_delete/<int:pk>', views.reply_delete, name="reply_delete"),
 ]
