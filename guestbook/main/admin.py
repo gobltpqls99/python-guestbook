ccb1b67e920e64ed348fe10f025375cf26187ab9
gobltpqls99 <gobltpqls99@naver.com>
2021-06-10 14:07:39 +0900

    login/signin

--- guestbook/main/admin.py
+++ guestbook/main/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Post, Reply
+from .models import Post, Reply, User
 
 
 admin.site.register(Post)
 admin.site.register(Reply)
+admin.site.register(User)
